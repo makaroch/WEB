c9e430d7a9bf35be7e6012f3a47c6eb9aa9c86f5
Alex <da.ek.ned@yandex.ru>
2023-04-27 21:23:48 +0300

    text

--- JS/s1/script.js
+++ JS/s1/script.js
@@ -7,26 +7,26 @@ const content = document.querySelector(".content");
 
 function plas() {
     const button = document.querySelector(".button");
-    let x =  Number(button.textContent);     
+    let x = Number(button.textContent);
     x++;
-    button.textContent = x;  
+    button.textContent = x;
 }
 //elem.classList.add/remove("class") 
 
 let flag = true;
 function tema() {
-    
-    if (flag){
+
+    if (flag) {
         const body = document.querySelector(".body_nait");
         body.classList.remove("body_nait");
         body.classList.add("body_day");
-        flag = ! flag;
+        flag = !flag;
     }
-    else{
+    else {
         const body = document.querySelector(".body_day");
         body.classList.remove("body_day");
         body.classList.add("body_nait");
-        flag = ! flag;
+        flag = !flag;
     }
 }
 
@@ -41,24 +41,62 @@ function tema() {
 // }
 const button = document.querySelector('.num');
 button.addEventListener('click', function () {
-  console.log('click');
+    console.log('click');
 });
 
 
 function randNum() {
-    document.querySelector(".num").textContent = Math.round(Math.random()*100);
+    document.querySelector(".num").textContent = Math.round(Math.random() * 100);
 }
 
 let flag1 = true;
 function f1() {
-    if (flag1){
+    if (flag1) {
         const body = document.querySelector(".text");
         body.classList.add("text_non");
-        flag1 = ! flag1;
+        flag1 = !flag1;
     }
-    else{
+    else {
         const body = document.querySelector(".text");
         body.classList.remove("text_non");
-        flag1 = ! flag1;
+        flag1 = !flag1;
     }
+}
+
+function b1() {
+    document.querySelector(".b1").classList.add("activ_button");
+    document.querySelector(".b2").classList.remove("activ_button");
+    document.querySelector(".b3").classList.remove("activ_button");
+    const t1 = document.querySelector(".text1");
+    t1.classList.remove("text_non");
+    const t2 = document.querySelector(".text2");
+    t2.classList.add("text_non");
+    const t3 = document.querySelector(".text3");
+    t3.classList.add("text_non");
+}
+function b2() {
+    document.querySelector(".b1").classList.remove("activ_button");
+    document.querySelector(".b2").classList.add("activ_button");
+    document.querySelector(".b3").classList.remove("activ_button");
+
+    const t1 = document.querySelector(".text1");
+    t1.classList.add("text_non");
+
+    const t2 = document.querySelector(".text2");
+    t2.classList.remove("text_non");
+
+    const t3 = document.querySelector(".text3");
+    t3.classList.add("text_non");
+}
+function b3() {
+    document.querySelector(".b1").classList.remove("activ_button");
+    document.querySelector(".b2").classList.remove("activ_button");
+    document.querySelector(".b3").classList.add("activ_button");
+
+    const t1 = document.querySelector(".text1");
+    t1.classList.add("text_non");
+    const t2 = document.querySelector(".text2");
+    t2.classList.add("text_non");
+    const t3 = document.querySelector(".text3");
+    t3.classList.remove("text_non");
 }
